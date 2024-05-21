2b7ecee975ef5b3f179a0258c818812725e5fcf7
francislub <lubanjwafrancis@gmail.com>
2024-05-21 02:22:36 -0700

    added now

--- app/authconfig.js
+++ app/authconfig.js
@@ -5,14 +5,24 @@ export const authConfig = {
     },
     callbacks: {
         authorized({auth,request}) {
-            const isLoggedIn = auth?.user;
+            // const isLoggedIn = auth?.user;
+            // const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard")
+            // const isOnWebsite = request.nextUrl.pathname.startsWith("/website")
+            // if(isOnDashboard || isOnWebsite){
+            //     if(isLoggedIn) return true;
+            //     return false;
+            // }else if(isLoggedIn){ 
+            //     return Response.redirect(new URL("/website/home", request.nextUrl));
+            // }
+            // return true;
+            // const isLoggedIn = auth?.user;
             const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard")
-            const isOnWebsite = request.nextUrl.pathname.startsWith("/website")
-            if(isOnDashboard || isOnWebsite){
-                if(isLoggedIn) return true;
-                return false;
-            }else if(isLoggedIn){ 
-                return Response.redirect(new URL("/website", request.nextUrl));
+            const isOnHome = request.nextUrl.pathname.startsWith("/")
+            // const isOnWebsite = request.nextUrl.pathname.startsWith("/website")
+            if(isOnHome){
+                return Response.redirect(new URL("/website/home", request.nextUrl));
+            }else if(isOnDashboard){ 
+                return Response.redirect(new URL("/dashboard", request.nextUrl));
             }
             return true;
         },
