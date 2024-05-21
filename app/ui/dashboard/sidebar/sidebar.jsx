0f76a1a9c3e71856e44c41f4a3084d35d2b4c6bc
francislub <lubanjwafrancis@gmail.com>
2024-05-21 14:38:19 -0700

    auth

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -80,19 +80,21 @@ const menuItems = [
 ];
 
 const Sidebar = async () => {
-  const { user } = await auth();
+  // const { user } = await auth();
   return (
     <div className={styles.container}>
       <div className={styles.user}>
         <Image
           className={styles.userImage}
-          src={user.img || "/noavatar.png"}
+          // src={user.img || "/noavatar.png"}
+          src={"/noavatar.png"}
           alt=""
           width="50"
           height="50"
         />
         <div className={styles.userDetail}>
-          <span className={styles.username}>{user.username}</span>
+          {/* <span className={styles.username}>{user.username}</span> */}
+          <span className={styles.username}>Admin</span>
           <span className={styles.userTitle}>Administrator</span>
         </div>
       </div>
